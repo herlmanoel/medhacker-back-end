449408b064baf5c2c33d149936181620b8b3f0d4
herlmanoel <herlmanoelfernandes@gmail.com>
2021-03-20 17:13:08 -0300

    remove nodemon of start

--- src/server.js
+++ src/server.js
@@ -4,7 +4,6 @@ const app = express();
 app.use(express.json());
 
 app.use(async (req, res, next) => {
-
     await res.header("Access-Control-Allow-Origin", "*");
     await res.header("Access-Control-Allow-Credentials", "true");
     await res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
@@ -22,7 +21,9 @@ app.use(async (req, res, next) => {
 
 app.get('/ola', (req, res) => {
     return res.send("Ola");
-})
+});
+
+
 const port = process.env.PORT || 3333;
 
 app.listen(port, () => console.log('Servidor rodando'));
