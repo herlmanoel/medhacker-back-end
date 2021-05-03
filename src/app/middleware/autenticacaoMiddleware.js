05995aee159acd89839fee7b96fb8560fcf9426b
herlmanoel <herlmanoelfernandes@gmail.com>
2021-05-03 07:56:11 -0300

    uncommenting login middleware

--- src/app/middleware/autenticacaoMiddleware.js
+++ src/app/middleware/autenticacaoMiddleware.js
@@ -2,32 +2,32 @@ const jwt = require('jsonwebtoken');
 const autenticacaoConfig = require('../../config/autenticar.json');
 
 module.exports = (req, res, next) => {
-    next();
+    // next();
 
-    // const autenticacaoHeader = req.headers.authorization;
+    const autenticacaoHeader = req.headers.authorization;
 
-    // if (!autenticacaoHeader) {
-    //     return res.status(401).send({ error: 'Usuário não autorizado. Token não foi informado' })
-    // }
+    if (!autenticacaoHeader) {
+        return res.status(401).send({ error: 'Usuário não autorizado. Token não foi informado' })
+    }
 
-    // const partes = autenticacaoHeader.split(' ');
+    const partes = autenticacaoHeader.split(' ');
 
-    // if (!partes.length == 2) {
-    //     return res.status(401).send({ error: 'Token inválido' });
-    // }
+    if (!partes.length == 2) {
+        return res.status(401).send({ error: 'Token inválido' });
+    }
 
 
-    // const [scheme, token] = partes;
+    const [scheme, token] = partes;
 
-    // if (!/^Bearer$/i) {
-    //     return res.status(401).send({ error: 'Token mal formatado' });
-    // }
+    if (!/^Bearer$/i) {
+        return res.status(401).send({ error: 'Token mal formatado' });
+    }
 
-    // jwt.verify(token, autenticacaoConfig.secret, (err, decoded) => {
-    //     if (err) {
-    //         return res.status(401).send({ error: 'Token inválido jwt' });
-    //     }
-    //     res.userId = decoded.id;
-    //     return next();
-    // });
+    jwt.verify(token, autenticacaoConfig.secret, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: 'Token inválido jwt' });
+        }
+        res.userId = decoded.id;
+        return next();
+    });
 }
